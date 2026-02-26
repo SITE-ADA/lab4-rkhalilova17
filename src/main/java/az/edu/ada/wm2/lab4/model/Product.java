fcdc591f40671b0d2ad972c61ebc11d1069056d5
Rubabe <151638349+rkhalilova17@users.noreply.github.com>
2026-02-26 04:56:49 +0400

    Add Model Product

---
+++ src/main/java/az/edu/ada/wm2/lab4/model/Product.java
@@ -0,0 +1,62 @@
+package main.java.az.edu.ada.wm2.lab4.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Product {
+
+    private UUID id;
+    private String productName;
+    private BigDecimal price;
+    private LocalDate expirationDate;
+
+    public Product() {
+        this.id = UUID.randomUUID();
+    }
+
+    public Product(String productName, BigDecimal price, LocalDate expirationDate) {
+        this.id = UUID.randomUUID();
+        this.productName = productName;
+        this.price = price;
+        this.expirationDate = expirationDate;
+    }
+    public Product(UUID id, String productName, BigDecimal price, LocalDate expirationDate) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.expirationDate = expirationDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
